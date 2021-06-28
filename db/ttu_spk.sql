32067d897668945a9498a663a03cde3759ad59e6
joy ancell <joy.ancell30@gmail.com>
2021-06-28 21:36:19 +0800

    database

---
+++ db/ttu_spk.sql
@@ -0,0 +1,367 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Jun 2021 pada 15.35
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ttu_spk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `analisa_alternatif`
+--
+
+CREATE TABLE `analisa_alternatif` (
+  `alternatif_pertama` varchar(4) NOT NULL,
+  `nilai_analisa_alternatif` double NOT NULL,
+  `hasil_analisa_alternatif` double NOT NULL,
+  `alternatif_kedua` varchar(4) NOT NULL,
+  `id_kriteria` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `analisa_kriteria`
+--
+
+CREATE TABLE `analisa_kriteria` (
+  `kriteria_pertama` varchar(2) NOT NULL,
+  `nilai_analisa_kriteria` double NOT NULL,
+  `hasil_analisa_kriteria` double NOT NULL,
+  `kriteria_kedua` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_alternatif`
+--
+
+CREATE TABLE `data_alternatif` (
+  `id_alternatif` varchar(4) NOT NULL,
+  `nik` char(18) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `tempat_lahir` varchar(100) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `kelamin` enum('pria','wanita') NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jabatan` varchar(20) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pendidikan` varchar(20) NOT NULL,
+  `hasil_akhir` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_kriteria`
+--
+
+CREATE TABLE `data_kriteria` (
+  `id_kriteria` varchar(2) NOT NULL,
+  `nama_kriteria` varchar(45) NOT NULL,
+  `jumlah_kriteria` double NOT NULL,
+  `bobot_kriteria` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_kriteria`
+--
+
+INSERT INTO `data_kriteria` (`id_kriteria`, `nama_kriteria`, `jumlah_kriteria`, `bobot_kriteria`) VALUES
+('C1', 'WC Umum', 1.22222222222222, 0.7273253062726767),
+('C2', 'Akses Jalan', 10.11111111111111, 0.22116480011216966),
+('C3', 'Akomodasi', 19, 0.05150989361515634);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_wisata`
+--
+
+CREATE TABLE `data_wisata` (
+  `id_wisata` varchar(4) NOT NULL,
+  `nama_wisata` varchar(100) NOT NULL,
+  `foto_wisata` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `hasil_akhir` double DEFAULT NULL,
+  `lat` text NOT NULL,
+  `lng` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_wisata`
+--
+
+INSERT INTO `data_wisata` (`id_wisata`, `nama_wisata`, `foto_wisata`, `alamat`, `hasil_akhir`, `lat`, `lng`) VALUES
+('A001', 'wisata contoh', '1623487962.png', 'dsdsd sdsd', NULL, '', ''),
+('A002', 'sdsdsddsd', '513.jpg', 'dsdsd sdsd', NULL, '', ''),
+('A003', 'ffaf', '123.png', 'dsdsd sdsd', NULL, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jum_alt_kri`
+--
+
+CREATE TABLE `jum_alt_kri` (
+  `id_alternatif` varchar(4) NOT NULL,
+  `id_kriteria` varchar(2) NOT NULL,
+  `jumlah_alt_kri` double NOT NULL,
+  `skor_alt_kri` double NOT NULL,
+  `hasil_alt_kri` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `jum_nilai` double NOT NULL,
+  `ket_nilai` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `nilai`
+--
+
+INSERT INTO `nilai` (`id_nilai`, `jum_nilai`, `ket_nilai`) VALUES
+(2, 9, 'Mutlak sangat penting dari'),
+(3, 8, 'Mendekati mutlak dari'),
+(8, 7, 'Sangat penting dari'),
+(9, 6, 'Mendekati sangat penting dari'),
+(10, 5, 'Lebih penting dari'),
+(11, 4, 'Mendekati lebih penting dari'),
+(12, 3, 'Sedikit lebih penting dari'),
+(13, 2, 'Mendekati sedikit lebih penting dari'),
+(14, 1, 'Sama penting dengan'),
+(15, 0.5, '1 bagi mendekati sedikit lebih penting dari'),
+(16, 0.333, '1 bagi sedikit lebih penting dari'),
+(17, 0.25, '1 bagi mendekati lebih penting dari'),
+(18, 0.2, '1 bagi lebih penting dari'),
+(19, 0.167, '1 bagi mendekati sangat penting dari'),
+(20, 0.143, '1 bagi sangat penting dari'),
+(21, 0.125, '1 bagi mendekati mutlak dari'),
+(22, 0.1, '1 bagi mutlak sangat penting dari');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_awal`
+--
+
+CREATE TABLE `nilai_awal` (
+  `id_nilai_awal` int(11) NOT NULL,
+  `id_alternatif` varchar(4) NOT NULL,
+  `nilai` varchar(5) NOT NULL,
+  `keterangan` enum('B','C','K') NOT NULL,
+  `periode` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_awal_detail`
+--
+
+CREATE TABLE `nilai_awal_detail` (
+  `id_nilai_awal_detail` int(11) NOT NULL,
+  `id_nilai_awal` int(11) NOT NULL,
+  `id_kriteria` varchar(2) NOT NULL,
+  `nilai` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `role` enum('atasan','kepegawaian','manajer') NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengguna`
+--
+
+INSERT INTO `pengguna` (`id_pengguna`, `nama_lengkap`, `role`, `username`, `password`) VALUES
+(2, 'nama admin', 'atasan', 'admin', '827ccb0eea8a706c4c34a16891f84e7b');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ranking`
+--
+
+CREATE TABLE `ranking` (
+  `kriteria` varchar(2) NOT NULL,
+  `skor_bobot` double NOT NULL,
+  `alternatif` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `analisa_alternatif`
+--
+ALTER TABLE `analisa_alternatif`
+  ADD PRIMARY KEY (`alternatif_pertama`,`alternatif_kedua`,`id_kriteria`),
+  ADD KEY `alternatif_kedua` (`alternatif_kedua`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `analisa_kriteria`
+--
+ALTER TABLE `analisa_kriteria`
+  ADD PRIMARY KEY (`kriteria_pertama`,`kriteria_kedua`),
+  ADD KEY `kriteria_kedua` (`kriteria_kedua`);
+
+--
+-- Indeks untuk tabel `data_alternatif`
+--
+ALTER TABLE `data_alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indeks untuk tabel `data_kriteria`
+--
+ALTER TABLE `data_kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `data_wisata`
+--
+ALTER TABLE `data_wisata`
+  ADD PRIMARY KEY (`id_wisata`);
+
+--
+-- Indeks untuk tabel `jum_alt_kri`
+--
+ALTER TABLE `jum_alt_kri`
+  ADD PRIMARY KEY (`id_alternatif`,`id_kriteria`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id_nilai`);
+
+--
+-- Indeks untuk tabel `nilai_awal`
+--
+ALTER TABLE `nilai_awal`
+  ADD PRIMARY KEY (`id_nilai_awal`,`id_alternatif`),
+  ADD KEY `alternatif` (`id_alternatif`);
+
+--
+-- Indeks untuk tabel `nilai_awal_detail`
+--
+ALTER TABLE `nilai_awal_detail`
+  ADD PRIMARY KEY (`id_nilai_awal_detail`,`id_nilai_awal`),
+  ADD KEY `alternatif` (`id_nilai_awal`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indeks untuk tabel `ranking`
+--
+ALTER TABLE `ranking`
+  ADD PRIMARY KEY (`kriteria`,`alternatif`),
+  ADD KEY `alternatif` (`alternatif`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai_awal`
+--
+ALTER TABLE `nilai_awal`
+  MODIFY `id_nilai_awal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai_awal_detail`
+--
+ALTER TABLE `nilai_awal_detail`
+  MODIFY `id_nilai_awal_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `analisa_alternatif`
+--
+ALTER TABLE `analisa_alternatif`
+  ADD CONSTRAINT `analisa_alternatif_ibfk_1` FOREIGN KEY (`id_kriteria`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `analisa_kriteria`
+--
+ALTER TABLE `analisa_kriteria`
+  ADD CONSTRAINT `analisa_kriteria_ibfk_1` FOREIGN KEY (`kriteria_pertama`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `analisa_kriteria_ibfk_2` FOREIGN KEY (`kriteria_kedua`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `jum_alt_kri`
+--
+ALTER TABLE `jum_alt_kri`
+  ADD CONSTRAINT `jum_alt_kri_ibfk_1` FOREIGN KEY (`id_kriteria`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `nilai_awal_detail`
+--
+ALTER TABLE `nilai_awal_detail`
+  ADD CONSTRAINT `nilai_awal_detail_ibfk_1` FOREIGN KEY (`id_kriteria`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nilai_awal_detail_ibfk_2` FOREIGN KEY (`id_nilai_awal`) REFERENCES `nilai_awal` (`id_nilai_awal`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `ranking`
+--
+ALTER TABLE `ranking`
+  ADD CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`kriteria`) REFERENCES `data_kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ranking_ibfk_2` FOREIGN KEY (`alternatif`) REFERENCES `data_alternatif` (`id_alternatif`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
